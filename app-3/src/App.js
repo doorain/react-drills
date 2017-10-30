1eb5088c16b03e40ca988974995d5d03576c27ee
Dorian Morgan <doorain@gmail.com>
2017-10-30 15:56:27 -0600

    1-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      array: ["hello dude", "whats's up bro?", "see ya"],
+      filteredList:''
+    }
+
+  }
+  updatedList(change){
+    this.setState({filteredList: change})
+  }
+
   render() {
+    let x = this.state.array.filter( (element, index) => {
+      return element.includes( this.state.filteredList);
+    }).map( (element, index) => {
+      return <h2>{ element }</h2>
+
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={ (e) => this.updatedList(e.target.value)} />
+
+        {x}
       </div>
     );
   }
